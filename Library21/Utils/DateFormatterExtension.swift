dccfe05ba1c3b0bfd273b9eae2dc597f929bc4c7
Extollite <42713788+Extollite@users.noreply.github.com>
2021-05-24 22:42:37 +0200

    Fix due date displaying (#10)

--- Library21/Utils/DateFormatterExtension.swift
+++ Library21/Utils/DateFormatterExtension.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 extension DateFormatter {
-  static let iso8601Full: DateFormatter = {
+  static let iso8601: DateFormatter = {
     let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
     formatter.calendar = Calendar(identifier: .iso8601)
     formatter.timeZone = TimeZone(secondsFromGMT: 0)
     formatter.locale = Locale(identifier: "en_US_POSIX")
